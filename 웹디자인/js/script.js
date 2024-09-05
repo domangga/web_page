862b522b46b0b88aacf2e8041dc4014c1aa818f2
도멩가 <121483261+domangga@users.noreply.github.com>
2024-09-05 11:14:34 +0900

    Add files via upload

--- 웹디자인/js/script.js
+++ 웹디자인/js/script.js
@@ -1,3 +1,10 @@
-window.onload=function(){
-    
-}
+$('#title > li').mouseover(function(){
+    $('.subtitle').css({left:'200px',opacity: '1',zIndex: '1'});
+  }).mouseout(function(){
+    $('.subtitle').css({left:'-100px',opacity: '0',zIndex: '-1'});
+  });
+  $('#title2 > li').mouseover(function(){
+    $('.subtitle').css({left:'200px',opacity: '1',zIndex: '1'});
+  }).mouseout(function(){
+    $('.subtitle').css({left:'-100px',opacity: '0',zIndex: '-1'});
+  });
